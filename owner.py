e71a42ced11343ae692b718cb3bcedf6a741f6f4
chiranirisaka <thanujanishshanka04@gmail.com>
2024-05-19 13:45:21 +0530

    Update owner.py

--- owner.py
+++ owner.py
@@ -1,21 +1,16 @@
-# owner.py
-
 from bus import Bus
 
+
 class Owner:
-    def __init__(self, owner_id, name):
-        self.owner_id = owner_id
-        self.name = name
+    def __init__(self):
         self.buses = []
 
-    def add_bus(self, bus):
+
+    def add_bus(self, bus_id, seats):
+        bus = Bus(bus_id, seats)
         self.buses.append(bus)
-        print(f"Bus {bus.bus_id} added by {self.name}")
+        print(f"Bus {bus_id} with {seats} seats added.")
 
     def view_buses(self):
         for bus in self.buses:
             print(bus)
-
-    def __str__(self):
-        return f"Owner ID: {self.owner_id}, Name: {self.name}"
- 
