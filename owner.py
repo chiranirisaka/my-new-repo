45e33bcc09e5f9a991bcd2fdfc98345d05d2cc4b
chiranirisaka <thanujanishshanka04@gmail.com>
2024-05-19 11:37:54 +0530

    Update owner.py

--- owner.py
+++ owner.py
@@ -1,1 +1,21 @@
-'print("This is the owner module")' 
+# owner.py
+
+from bus import Bus
+
+class Owner:
+    def __init__(self, owner_id, name):
+        self.owner_id = owner_id
+        self.name = name
+        self.buses = []
+
+    def add_bus(self, bus):
+        self.buses.append(bus)
+        print(f"Bus {bus.bus_id} added by {self.name}")
+
+    def view_buses(self):
+        for bus in self.buses:
+            print(bus)
+
+    def __str__(self):
+        return f"Owner ID: {self.owner_id}, Name: {self.name}"
+ 
